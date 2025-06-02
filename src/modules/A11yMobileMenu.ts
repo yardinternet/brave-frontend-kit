68bd28a6cc9538c10b771401e0c2d1824bb2cffa
Freek de Kruijff <freek.dekruijff@yard.nl>
2025-06-02 11:44:00 +0200

    (chore): setup A11yMobileMenu

---
+++ src/modules/A11yMobileMenu.ts
@@ -0,0 +1,189 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import * as focusTrap from 'focus-trap';
+import { checkCanFocusTrap } from '@utils/focus-trap.ts';
+
+interface A11yMobileMenuOptions {
+	selectorPrefix?: string;
+}
+
+export class A11yMobileMenu {
+	private focusTrapMobileMenu: focusTrap.FocusTrap | null = null;
+	private closeBtn: HTMLElement | null;
+	private hamburger: HTMLElement | null;
+	private mobileMenu: HTMLElement | null;
+	private expandableMenuItems: NodeListOf< Element >;
+	private focusTrapOptions: focusTrap.Options;
+
+	public readonly selectorPrefix: string;
+
+	constructor( options: A11yMobileMenuOptions = {} ) {
+		this.selectorPrefix = options.selectorPrefix || 'js-brave';
+
+		this.init();
+	}
+
+	private init() {
+		this.closeBtn = document.querySelector(
+			`#${ this.selectorPrefix }-mobile-menu-close-btn`
+		);
+		this.hamburger = document.querySelector(
+			`#${ this.selectorPrefix }-hamburger`
+		);
+		this.mobileMenu = document.querySelector(
+			`#${ this.selectorPrefix }-mobile-menu`
+		);
+		this.expandableMenuItems = document.querySelectorAll(
+			'.mobile-menu .menu-item-has-children'
+		);
+
+		this.focusTrapOptions = {
+			allowOutsideClick: true,
+			checkCanFocusTrap,
+			onActivate: () => this.onActivateFocusTrap(),
+			onDeactivate: () => this.onDeactivateFocusTrap(),
+		};
+
+		this.bindEvents();
+	}
+
+	private bindEvents(): void {
+		if ( ! this.closeBtn || ! this.hamburger || ! this.mobileMenu ) return;
+
+		this.focusTrapMobileMenu = focusTrap.createFocusTrap(
+			this.mobileMenu,
+			this.focusTrapOptions
+		);
+
+		this.closeBtn.addEventListener(
+			'click',
+			() => this.focusTrapMobileMenu?.deactivate()
+		);
+		this.hamburger.addEventListener( 'click', () => {
+			if ( this.focusTrapMobileMenu?.active ) {
+				this.focusTrapMobileMenu.deactivate();
+			} else {
+				this.focusTrapMobileMenu?.activate();
+			}
+		} );
+
+		this.initExpandableMenuItems();
+	}
+
+	/**
+	 * Show mobile menu. Lock body scroll and add correct aria attributes.
+	 */
+	private onActivateFocusTrap(): void {
+		if ( ! this.mobileMenu || ! this.hamburger ) return;
+
+		disableBodyScroll( this.mobileMenu );
+
+		this.hamburger.setAttribute( 'aria-expanded', 'true' );
+		this.hamburger.setAttribute( 'aria-label', 'Sluit menu' );
+		this.mobileMenu.setAttribute( 'aria-hidden', 'false' );
+
+		this.mobileMenu.animate(
+			[
+				{
+					transform: 'translateX(100%)',
+					opacity: '0',
+					visibility: 'hidden',
+				},
+				{
+					transform: 'translateX(0)',
+					opacity: '1',
+					visibility: 'visible',
+				},
+			],
+			{
+				duration: 500,
+				easing: 'cubic-bezier(0.22,1,0.36,1)',
+				fill: 'both',
+			}
+		);
+	}
+
+	/**
+	 * Hide mobile menu. Unlock body scroll and add correct aria attributes.
+	 */
+	private onDeactivateFocusTrap(): void {
+		if ( ! this.mobileMenu || ! this.hamburger ) return;
+
+		enableBodyScroll( this.mobileMenu );
+
+		this.closeAllExpandableMenuItems();
+
+		this.hamburger.setAttribute( 'aria-expanded', 'false' );
+		this.hamburger.setAttribute( 'aria-label', 'Open menu' );
+		this.mobileMenu.setAttribute( 'aria-hidden', 'true' );
+
+		this.mobileMenu.animate(
+			[
+				{
+					transform: 'translateX(0)',
+					opacity: '1',
+					visibility: 'visible',
+				},
+				{
+					transform: 'translateX(100%)',
+					opacity: '0',
+					visibility: 'hidden',
+				},
+			],
+			{
+				duration: 500,
+				easing: 'ease-out',
+				fill: 'both',
+			}
+		);
+	}
+
+	/**
+	 * Initialize expandable menu items and add necessary aria attributes.
+	 */
+	private initExpandableMenuItems(): void {
+		this.expandableMenuItems.forEach( ( item ) => {
+			const link = item.querySelector( 'a' );
+			if ( ! link ) return;
+
+			link.setAttribute( 'aria-haspopup', 'true' );
+			link.setAttribute( 'aria-expanded', 'false' );
+
+			link.addEventListener( 'click', ( event ) =>
+				this.onClickExpandableMenuItem(
+					event,
+					item,
+					link as HTMLElement
+				)
+			);
+		} );
+	}
+
+	/**
+	 * Handle click event. Prevent opening link, add aria-expanded and toggle class.
+	 */
+	private onClickExpandableMenuItem(
+		event: Event,
+		item: Element,
+		link: HTMLElement
+	): void {
+		event.preventDefault();
+
+		const isOpen = link.getAttribute( 'aria-expanded' ) === 'true';
+		link.setAttribute( 'aria-expanded', String( ! isOpen ) );
+
+		item.classList.toggle( 'show-sub-menu' );
+	}
+
+	/**
+	 * Close all expandable menu items. Set aria-expanded to false and remove active class.
+	 */
+	private closeAllExpandableMenuItems(): void {
+		this.expandableMenuItems.forEach( ( item ) => {
+			const link = item.querySelector( 'a' );
+			if ( link ) {
+				link.setAttribute( 'aria-expanded', 'false' );
+			}
+			item.classList.remove( 'show-sub-menu' );
+		} );
+	}
+}

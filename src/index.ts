f2164374593779289a3ea2f07340eb60fe652b8d
Wybe van den Bosch <wybe.vandenbosch+git@gmail.com>
2026-01-27 17:39:27 +0100

    fix: actually export dialog

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Accordion } from '@modules/Accordion';
 import { A11yCards } from '@modules/A11yCards';
 import { A11yFacetWP } from '@modules/A11yFacetWP.ts';
 import { A11yMobileMenu } from '@modules/A11yMobileMenu.ts';
+import { Dialog } from '@modules/Dialog.ts';
 import { EnhanceExternalLinks } from '@modules/EnhanceExternalLinks';
 import { EnhancePDFLinks } from '@modules/EnhancePDFLinks';
 import { FocusStyle } from '@modules/FocusStyle';
@@ -18,6 +19,7 @@ export {
 	A11yCards,
 	A11yFacetWP,
 	A11yMobileMenu,
+	Dialog,
 	EnhanceExternalLinks,
 	EnhancePDFLinks,
 	FocusStyle,

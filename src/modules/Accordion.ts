88285d49210602d0783fdc9fb80a286c64abfed9
Wybe van den Bosch <wybe.vandenbosch+git@gmail.com>
2026-01-27 13:53:54 +0100

    fix: refactor file after pr feedback

--- src/modules/Accordion.ts
+++ src/modules/Accordion.ts
@@ -4,80 +4,64 @@
 import AccordionJS from 'accordion-js';
 
 export class Accordion {
-	ACCORDION_CLASS = '.accordion-wrapper';
-	ACCORDION_ITEM_CLASS = '.ac';
-	ACCORDION_DEFAULT_OPEN_CLASS = 'is-open';
-	ACCORDION_PANEL_CLASS = '.ac-panel';
-	ACCORDION_HIDDEN_CLASS = 'hidden';
+	private readonly WRAPPER_SELECTOR = '.accordion-wrapper';
+	private readonly ITEM_SELECTOR = '.ac';
+	private readonly PANEL_SELECTOR = '.ac-panel';
+	private readonly DEFAULT_OPEN_CLASS = 'is-open';
+	private readonly HIDDEN_CLASS = 'hidden';
 
 	constructor() {
-		const accordions = this.searchForAccordions();
-		if ( ! accordions ) return;
+		const accordions = this.getAccordionWrappers();
+		if ( accordions.length === 0 ) return;
 
-		accordions.forEach( ( accordion ) => {
-			this.initSpecificAccordion( accordion );
-		} );
+		accordions.forEach( this.initAccordion, this );
 	}
 
 	/**
-	 * Helper Functions
+	 * Main Function, Use the is-open class on .ac elements to define open accordion items on init.
 	 */
-	searchForAccordions(): HTMLDivElement[] {
-		const accordions = document.querySelectorAll< HTMLDivElement >(
-			this.ACCORDION_CLASS
-		);
-		return Array.from( accordions );
-	}
-
-	searchWithinAccordionForItems( accordion: Element ): HTMLElement[] {
-		const items = accordion.querySelectorAll< HTMLElement >(
-			this.ACCORDION_ITEM_CLASS
-		);
-		return Array.from( items );
-	}
-
-	searchForDefaultOpenItems( items: HTMLElement[] ): number[] {
-		const defaultOpenIndexes: number[] = [];
-		if ( ! items || items.length === 0 ) return defaultOpenIndexes;
-
-		items.forEach( ( item, index ) => {
-			if (
-				item.classList.contains( this.ACCORDION_DEFAULT_OPEN_CLASS )
-			) {
-				defaultOpenIndexes.push( index );
-			}
-		} );
-
-		return defaultOpenIndexes;
-	}
-
-	/**
-	 * Main Function : Initializes the accordion.
-	 * Use the is-open class on .accordion-wrapper to define an open accordion on init.
-	 */
-	initSpecificAccordion( accordion: Element ): void {
-		const items = this.searchWithinAccordionForItems( accordion );
+	private initAccordion( accordion: HTMLElement ): void {
+		const items = this.getAccordionItems( accordion );
 		if ( ! items || items.length === 0 ) return;
 
 		// https://github.com/michu2k/Accordion?tab=readme-ov-file#options
 		const accordionOptions = {
-			showMultiple:
-				( accordion as HTMLElement ).dataset.multiple === 'true',
-			openOnInit: this.searchForDefaultOpenItems( items ),
+			showMultiple: accordion.dataset.multiple === 'true',
+			openOnInit: this.getDefaultOpenItemIndexes( items ),
 			ariaEnabled: true,
 			duration: 400,
 			beforeOpen: ( item: HTMLElement ): void => {
 				item
-					.querySelector( this.ACCORDION_PANEL_CLASS )
-					?.classList.remove( this.ACCORDION_HIDDEN_CLASS );
+					.querySelector( this.PANEL_SELECTOR )
+					?.classList.remove( this.HIDDEN_CLASS );
 			},
 			onClose: ( item: HTMLElement ): void => {
 				item
-					.querySelector( this.ACCORDION_PANEL_CLASS )
-					?.classList.add( this.ACCORDION_HIDDEN_CLASS );
+					.querySelector( this.PANEL_SELECTOR )
+					?.classList.add( this.HIDDEN_CLASS );
 			},
 		};
 
 		new AccordionJS( accordion, accordionOptions );
+	}
+
+	private getAccordionWrappers(): HTMLDivElement[] {
+		const accordions = document.querySelectorAll< HTMLDivElement >(
+			this.WRAPPER_SELECTOR
+		);
+		return Array.from( accordions );
+	}
+
+	private getAccordionItems( accordion: HTMLElement ): HTMLElement[] {
+		const items = accordion.querySelectorAll< HTMLElement >(
+			this.ITEM_SELECTOR
+		);
+		return Array.from( items );
+	}
+
+	private getDefaultOpenItemIndexes( items: HTMLElement[] ): number[] {
+		return items.flatMap( ( item, index ) =>
+			item.classList.contains( this.DEFAULT_OPEN_CLASS ) ? [ index ] : []
+		);
 	}
 }
